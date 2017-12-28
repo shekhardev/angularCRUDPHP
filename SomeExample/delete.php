ee9820a7f4975a11db14bc8e83647370ee3bb9ba
shekhar <shekhar.lapp@gmail.com>
2017-12-28 17:59:31 +0530

    initial commit

---
+++ SomeExample/delete.php
@@ -0,0 +1,17 @@
+<?php
+
+$connect = mysqli_connect("localhost", "root", "sekhar", "ajaxdb");  
+ $data = json_decode(file_get_contents("php://input"));  
+ if(count($data) > 0)  
+ {  
+      $id = $data->id;  
+      $query = "DELETE FROM tbl_user WHERE id='$id'";  
+      if(mysqli_query($connect, $query))  
+      {  
+           echo 'Data Deleted';  
+      }  
+      else  
+      {  
+           echo 'Error';  
+      }  
+ }  
---
+++ SomeExample/select.php
@@ -0,0 +1,14 @@
+<?php
+
+$connect = mysqli_connect("localhost", "root", "sekhar", "ajaxdb");  
+ $output = array();  
+ $query = "SELECT * FROM tbl_user";  
+ $result = mysqli_query($connect, $query);  
+ if(mysqli_num_rows($result) > 0)  
+ {  
+      while($row = mysqli_fetch_array($result))  
+      {  
+           $output[] = $row;  
+      }  
+      echo json_encode($output);  
+ }  
